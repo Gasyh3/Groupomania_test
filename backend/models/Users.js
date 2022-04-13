cea45b9233281066b726a83009907cbb3a5febd6
Gasyh3 <kevrakt@gmail.com>
2022-04-13 11:58:40 +0200

    Mise à jour de Users

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -4,9 +4,27 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      default: false,
     },
   });
 
@@ -16,6 +34,10 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     Users.hasMany(models.Posts, {
+      onDelete: "cascade",
+    });
+
+    Users.hasMany(models.Comments, {
       onDelete: "cascade",
     });
   };
